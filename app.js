c4b5b9c79da6e9de657948b37a63b46b8d5c9ec8
Steve Benton <steve@bentonemail.uk>
2026-09-08 12:25:52 +0100

    add multiple server responses

--- app.js
+++ app.js
@@ -3,8 +3,16 @@
 const http = require("http");
 
 function handleRequest(request, response) {
-  response.statusCode = 200;
-  response.end("<h1>Hello World! - from my server!</h1>");
+  // localhost:3000/currenttime
+  // localhost:3000
+
+  if (request.url === "/currenttime") {
+    response.statusCode = 200;
+    response.end("<h1>" + new Date().toISOString() + "</h1>");
+  } else if ((request.url = "/")) {
+    response.statusCode = 200;
+    response.end("<h1>Hello World! - from my server!</h1>");
+  }
 }
 
 const server = http.createServer(handleRequest);
