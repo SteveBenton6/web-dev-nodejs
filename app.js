a7c8e8a52ddf0d43b5c08e803378c564b2e5d3ba
Steve Benton <steve@bentonemail.uk>
2026-09-09 14:58:14 +0100

    add post handling to receive name from website

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/currenttime", function (req, res) {
   res.send("<h1>" + new Date().toISOString() + "</h1>");
 }); // localhost:3000/currenttime
 
 app.get("/", function (req, res) {
   res.send(
-    "<form action='/store-user' method='POST'><label>Your Name </label><input type = 'text'><button>Submit</button></form>",
+    "<form action='/store-user' method='POST'><label>Your Name </label><input type = 'text' name='username'><button>Submit</button></form>",
   );
 }); // localhost:3000/
+
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+  console.log(userName);
+  res.send("<h2>Username stored!</h2>");
+});
 
 app.listen(3000);
